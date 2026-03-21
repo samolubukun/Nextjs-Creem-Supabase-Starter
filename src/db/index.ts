74d28b698bfa6359101443c08a7c62bbb43240cb
Olubukun Samuel <samuelolubukun@gmail.com>
2026-03-21 06:28:09 +0100

    feat: setup drizzle orm (src/db/index.ts, drizzle.config.ts)

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+
+const connectionString = process.env.DATABASE_URL!;
+
+// For query purposes
+const client = postgres(connectionString);
+export const db = drizzle(client, { schema });
+
+// For migrations and one-off connections
+export const migrationClient = postgres(connectionString, { max: 1 });
